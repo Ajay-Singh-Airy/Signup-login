cfd360ab6cebd4512c993141384076a48fee3d9a
Ajay Singh <167549325+Ajay-Singh-Airy@users.noreply.github.com>
2024-04-20 23:09:39 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,15 @@
+
+ <?php 
+
+
+$dbhost = "127.0.0.1:3307";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "login_sample_db";
+
+if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+{
+	die("failed to connect!");
+}
+
+
